a5446c314abe64a2e729e23716593e341ae49c0a
Deniskerec <ddeniskerec@gmail.com>
2020-01-06 17:53:48 +0100

    test file

--- Client/src/Blog/posts.js
+++ Client/src/Blog/posts.js
@@ -1,20 +1,3 @@
-import React, { useContext, useEffect, useState } from 'react';
-
-import { Link } from 'react-router-dom';
-
-import axios from 'axios';
-import moment from 'moment';
-import Context from '../utils/context';
-
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-
-import '../App.css';
-import '../styles/pagination.css';
 
 
 
